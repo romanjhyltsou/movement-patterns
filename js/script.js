c95ea7de23eae4d2260bc42ce0ea9c1ee7be570f
romanjhyltsou <romanjhyltsou@gmail.com>
2022-12-02 00:38:26 +0300

    init: start movement-patterns

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d'); //default width 300px height 150px
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
+let gameSpeed = 5;
+
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = 'assets/img/layer-1.png';
+
+    
+    function animate() {
+      ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+      requestAnimationFrame(animate);
+      
+    }
+    animate();
+ */
